660736509ad7efd29d8afb3464111463a2ef43f2
Mersi <148212560+Mersirasyidah@users.noreply.github.com>
2026-07-15 15:03:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-import os
-import pandas as pd
-
-DATABASE = "data/database_scheduler.xlsx"
-
-print("Folder kerja :", os.getcwd())
-print("File ada :", os.path.exists(DATABASE))
-print("Lokasi :", DATABASE)
-
-excel = pd.ExcelFile(DATABASE)
+import streamlit as st
+from database import load_database
 
 # Konfigurasi halaman
 st.set_page_config(
@@ -35,28 +27,14 @@ for sheet in db.keys():
 
 st.divider()
 
-# Statistik Dashboard
+# Dashboard
 st.header("📊 Dashboard")
 
 col1, col2, col3 = st.columns(3)
 
-# Jumlah guru
-if "Guru" in db:
-    jumlah_guru = len(db["Guru"])
-else:
-    jumlah_guru = 0
-
-# Jumlah mapel
-if "Mapel" in db:
-    jumlah_mapel = len(db["Mapel"])
-else:
-    jumlah_mapel = 0
-
-# Jumlah rombel
-if "Rombel" in db:
-    jumlah_rombel = len(db["Rombel"])
-else:
-    jumlah_rombel = 0
+jumlah_guru = len(db["Guru"]) if "Guru" in db else 0
+jumlah_mapel = len(db["Mapel"]) if "Mapel" in db else 0
+jumlah_rombel = len(db["Rombel"]) if "Rombel" in db else 0
 
 col1.metric("👨‍🏫 Guru", jumlah_guru)
 col2.metric("📖 Mata Pelajaran", jumlah_mapel)
@@ -64,9 +42,6 @@ col3.metric("🏫 Rombel", jumlah_rombel)
 
 st.divider()
 
-# Preview Data Guru
 if "Guru" in db:
-
     st.subheader("Preview Data Guru")
-
     st.dataframe(db["Guru"], use_container_width=True)
