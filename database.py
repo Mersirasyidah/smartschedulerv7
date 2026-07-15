6bd59bbe2f43eebf30129d2062529c47ee90ab3b
Mersi <148212560+Mersirasyidah@users.noreply.github.com>
2026-07-15 14:32:47 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,28 @@
+import pandas as pd
 
+DATABASE = "data/database_scheduler.xlsx"
+
+
+def load_database():
+    """
+    Membaca seluruh sheet pada file Excel
+    """
+
+    data = {}
+
+    excel = pd.ExcelFile(DATABASE)
+
+    for sheet in excel.sheet_names:
+        data[sheet] = pd.read_excel(DATABASE, sheet_name=sheet)
+
+    return data
+
+
+if __name__ == "__main__":
+
+    database = load_database()
+
+    print("===== SHEET YANG DITEMUKAN =====")
+
+    for nama_sheet in database:
+        print(nama_sheet)
