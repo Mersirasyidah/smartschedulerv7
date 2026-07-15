162b6a829b10017b4f9865c287e8b3586b76fed7
Mersi <148212560+Mersirasyidah@users.noreply.github.com>
2026-07-15 14:40:21 +0700

    Update 2_👨‍🏫_Guru.py

--- pages/2_👨‍🏫_Guru.py
+++ pages/2_👨‍🏫_Guru.py
@@ -1,1 +1,42 @@
+import streamlit as st
+from database import load_database
 
+st.set_page_config(
+    page_title="Data Guru",
+    page_icon="👨‍🏫",
+    layout="wide"
+)
+
+db = load_database()
+
+st.title("👨‍🏫 Data Guru")
+
+if "Guru" in db:
+
+    guru = db["Guru"]
+
+    st.metric("Jumlah Guru", len(guru))
+
+    keyword = st.text_input(
+        "Cari Nama Guru"
+    )
+
+    if keyword:
+
+        guru = guru[
+            guru["Nama Guru"].str.contains(
+                keyword,
+                case=False,
+                na=False
+            )
+        ]
+
+    st.dataframe(
+        guru,
+        use_container_width=True,
+        hide_index=True
+    )
+
+else:
+
+    st.error("Sheet Guru tidak ditemukan")
