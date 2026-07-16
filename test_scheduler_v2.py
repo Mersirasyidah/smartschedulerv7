a076850433b341c376907013c080a73addb1e342
Mersi <148212560+Mersirasyidah@users.noreply.github.com>
2026-07-16 18:00:44 +0700

    Update test_scheduler_v2.py

--- test_scheduler_v2.py
+++ test_scheduler_v2.py
@@ -1,1 +1,28 @@
+from database import load_database
+from scheduler.scheduler import Scheduler
 
+print("Membaca database...")
+
+db = load_database()
+
+print("Database OK")
+
+engine = Scheduler(db)
+
+engine.prepare()
+
+print("Engine siap")
+
+hasil = engine.solve()
+
+print("Status Solver :", hasil)
+
+if hasil:
+
+    df = engine.dataframe()
+
+    print(df.head())
+
+else:
+
+    print("Tidak ada solusi.")
