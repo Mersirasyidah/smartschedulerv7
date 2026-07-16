dfba5dc850f72cb4347fe4e349b2d39bf82b6dd9
Mersi <148212560+Mersirasyidah@users.noreply.github.com>
2026-07-16 11:28:26 +0700

    Update 5_📅_MGMP.py

--- pages/5_📅_MGMP.py
+++ pages/5_📅_MGMP.py
@@ -1,7 +1,21 @@
-[
-'ID Guru',
-'Nama Guru',
-'Status',
-'Hari MGMP',
-'Prioritas'
-]
+import streamlit as st
+from database import load_database
+
+st.title("📅 Hari MGMP Guru")
+
+db = load_database()
+
+guru = db["Guru"]
+
+st.dataframe(
+    guru[
+        [
+            "ID Guru",
+            "Nama Guru",
+            "Hari MGMP",
+            "Prioritas"
+        ]
+    ],
+    use_container_width=True,
+    hide_index=True
+)
