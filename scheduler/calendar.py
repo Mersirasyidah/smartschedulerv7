a9622d7bca0e5c1007b16c9a89d7bf6311592083
Mersi <148212560+Mersirasyidah@users.noreply.github.com>
2026-07-16 17:51:11 +0700

    Update calendar.py

--- scheduler/calendar.py
+++ scheduler/calendar.py
@@ -1,1 +1,35 @@
+class CalendarEngine:
 
+    def __init__(self, loader):
+
+        self.loader = loader
+
+        self.slot = []
+
+
+    def build(self):
+
+        nomor = 0
+
+        df = self.loader.hari_jam
+
+
+        for _, row in df.iterrows():
+
+            if row["Jenis"] != "Pembelajaran":
+                continue
+
+            self.slot.append({
+
+                "id": nomor,
+
+                "hari": row["Hari"],
+
+                "jam": int(row["Jam"])
+
+            })
+
+            nomor += 1
+
+
+        print("Slot Pembelajaran :", len(self.slot))
